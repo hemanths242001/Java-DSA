509d86eb5a6eb87de197286d89678d7c237b34b5
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-04-01 21:02:04 +0530

    linear search commit

--- JavaDSA/src/javaDSA/LinearSearchArray.java
+++ JavaDSA/src/javaDSA/LinearSearchArray.java
@@ -1,9 +1,28 @@
 package javaDSA;
 
 public class LinearSearchArray {
+	
+	static int search(int arr[], int key) {
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] == key) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
 	public static void main(String[] args) {
+		int[] arr = {5, 10, 15, 20, 25};
+		int key = 15;
 		
+		int index = search(arr, key);
+		
+		if(index != -1) {
+			System.out.println(key+"found at the index of"+index);
+		}
+		else {
+			System.out.println("the element is not found");
+		}
 	}
 
 }
