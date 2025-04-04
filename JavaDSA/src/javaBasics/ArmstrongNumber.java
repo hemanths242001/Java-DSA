1855c287a88f37495bc6a82e0e54617715f0fce3
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-04-04 13:22:45 +0530

    armstrong commit

---
+++ JavaDSA/src/javaBasics/ArmstrongNumber.java
@@ -0,0 +1,31 @@
+package javaBasics;
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+	
+	static void checkArmstrong(int num) {
+		int temp = num;
+		int sum = 0;
+		int r;
+		while(num > 0) {
+			r = num % 10;
+			sum = sum + r*r*r;
+			num = num / 10;
+		}
+		if(temp == sum) {
+			System.out.println(num+" is a armstrong number");
+		}
+		else {
+			System.out.println(num+" is not a armstrong number");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number");
+		int num = sc.nextInt();
+		checkArmstrong(num);
+		sc.close();
+	}
+
+}
