26998699643aec2798dd966e4b9fcf058b7cd669
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-04-09 10:36:13 +0530

    .

--- JavaDSA/src/javaDSA/DeletingElementArray.java
+++ JavaDSA/src/javaDSA/DeletingElementArray.java
@@ -29,7 +29,7 @@ public class DeletingElementArray {
 
 	 // Driver's Code
 	 public static void main(String args[]){
-		 int arr[] = {10,20,30,40,50};
+		 int arr[] = {10,20,30,40,50,60,70};
 	     int n = arr.length;
 	     for(int x : arr) {
 	    	 System.out.print(x+" ");
