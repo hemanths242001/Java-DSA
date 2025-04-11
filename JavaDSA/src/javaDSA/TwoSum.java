6157aada62627067d2159b1d54b36893086a79be
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-04-12 00:15:47 +0530

    .

---
+++ JavaDSA/src/javaDSA/TwoSum.java
@@ -0,0 +1,37 @@
+package javaDSA;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int arr[] = {2,3,9,23,54};
+		int target = 5;
+		
+		int result[] = twoSumIndices(arr, target);
+		
+		if(result[0] == -1) {
+			System.out.println("the sum is not found");
+		}
+		else {
+			System.out.println("the indices are: "+result[0]+","+result[1]);
+		}
+		
+	}
+	public static int[] twoSumIndices(int arr[], int target) {
+		int left = 0;
+		int right = arr.length - 1;
+		
+		while(left < right) {
+			int sum = arr[left] + arr[right];
+			if(sum == target) {
+				return new int[] {left, right};
+			}
+			else if(sum < target) {
+				left++;
+			}
+			else {
+				right--;
+			}
+		}
+		return new int[] {-1, -1};
+	}
+}
