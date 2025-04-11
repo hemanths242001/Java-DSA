4da182c8c274e7d329af3ac17dd79047396ff1ee
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-04-11 17:41:09 +0530

    .

---
+++ JavaDSA/src/javaDSA/BinarySearchOccurrences.java
@@ -0,0 +1,60 @@
+package javaDSA;
+
+public class BinarySearchOccurrences {
+
+	public static void main(String[] args) {
+		int arr[] = {2, 4, 4, 4, 4, 6, 8};
+		System.out.println("Enter the target element");
+		int target = 4;
+		
+		System.out.println("First Occurrence: "+firstOccurrence(arr, target));
+		System.out.println("Last Occurrence: "+lastOccurrence(arr,target));
+		
+	}
+	public static int firstOccurrence(int arr[], int target) {
+		
+		int low = 0; // start index
+		int high = arr.length-1; // end index
+		int result = -1;
+		
+		while(low <= high) {
+			int middle = low + (high-low) / 2;
+			int value = arr[middle];
+			
+			if(value < target) {
+				low = middle + 1;
+			}
+			else if(value > target) {
+				high = middle - 1;
+			}
+			else {
+				result = middle;
+				high = middle - 1;
+			}
+		}
+		return result; // if it is not found
+	}
+	public static int lastOccurrence(int arr[], int target) {
+		
+		int low = 0; // start index
+		int high = arr.length-1; // end index
+		int result = -1;
+		
+		while(low <= high) {
+			int middle = low + (high-low) / 2;
+			int value = arr[middle];
+			
+			if(value < target) {
+				low = middle + 1;
+			}
+			else if(value > target) {
+				high = middle - 1;
+			}
+			else {
+				result = middle;
+				low = middle + 1;
+			}
+		}
+		return result; // if it is not found
+	}
+}
