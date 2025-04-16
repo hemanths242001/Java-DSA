beca0f88ca8d187296485b75ca5256bcdbcf3c11
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-04-16 19:26:32 +0530

    .

---
+++ JavaDSA/src/javaDSA/RotateArrayWithKSteps.java
@@ -0,0 +1,29 @@
+package javaDSA;
+
+import java.util.Arrays;
+
+public class RotateArrayWithKSteps {
+
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5,6,7,8};
+		int k = 3;
+		
+		System.out.println(Arrays.toString(rotate(arr, k)));
+	}
+	private static int[] rotate(int arr[], int k) {
+		int n = arr.length;
+		reverse(arr, 0, n-1);
+		reverse(arr, 0, k-1);
+		reverse(arr, k, n-1);
+		return arr;
+	}
+	private static void reverse(int arr[], int start, int end) {
+		while(start <= end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}
+}
