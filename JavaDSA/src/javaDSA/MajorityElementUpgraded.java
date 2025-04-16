32953523c83fc9bb580029f6b8eedb1ee23a299b
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-04-16 13:37:34 +0530

    .

---
+++ JavaDSA/src/javaDSA/MajorityElementUpgraded.java
@@ -0,0 +1,13 @@
+package javaDSA;
+
+import java.util.List;
+
+public class MajorityElementUpgraded {
+
+	public static void main(String[] args) {
+		// here majority element will be greater than n / 3
+		int arr[] = {2,2,3,3,2,2,3,3,3,3};
+		
+	}
+	
+}
