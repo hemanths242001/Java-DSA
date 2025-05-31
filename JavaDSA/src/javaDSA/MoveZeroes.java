5a064f18816b23f7d194034de3466ae8dc048132
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-06-01 01:48:04 +0530

    .

--- JavaDSA/src/javaDSA/MoveZeroes.java
+++ JavaDSA/src/javaDSA/MoveZeroes.java
@@ -1,9 +1,23 @@
 package javaDSA;
 
+import java.util.Arrays;
+
 public class MoveZeroes {
 
 	public static void main(String[] args) {
-		
+		int nums[] = {0,1,0,3,12};
+		zeroesToEnd(nums);
+		System.out.println(Arrays.toString(nums));
 	}
-
+	public static void zeroesToEnd(int nums[]) {
+		int nonZeroIndex = 0;
+		for(int i=0; i < nums.length; i++) {
+			if(nums[i] != 0) {
+				nums[nonZeroIndex++] = nums[i];
+			}
+		}
+		while(nonZeroIndex < nums.length) {
+			nums[nonZeroIndex++] = 0;
+		}
+	}
 }
