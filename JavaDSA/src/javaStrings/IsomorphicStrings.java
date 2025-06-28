e73603b4ae0c0f6990c04dfaee95b6e035b5d1f4
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-06-28 17:23:59 +0530

    committed

---
+++ JavaDSA/src/javaStrings/IsomorphicStrings.java
@@ -0,0 +1,29 @@
+package javaStrings;
+import java.util.*;
+
+public class IsomorphicStrings {
+
+	public static void main(String[] args) {
+		String s = "egg";
+		String t = "add";
+		System.out.println(isIsomorphic(s,t));
+	}
+	public static boolean isIsomorphic(String s, String t) {
+		HashMap<Character, Character> map1 = new HashMap<>();
+		HashMap<Character, Character> map2 = new HashMap<>();
+		
+        for(int i=0; i<s.length(); i++) {
+        	char a = s.charAt(i);
+        	char b = t.charAt(i);
+        	if(map1.containsKey(a) && map1.get(a) != b || map2.containsKey(b) && map2.get(b) != a) {
+        		return false;
+        	}
+        	map1.put(a, b);
+        	map2.put(b, a);
+        	
+        	
+        }
+        return true;
+    }
+
+}
